d7faa293f8922c95f9f20b99b90affdeff9f5efd
devhimelali <dev.himelali@gmail.com>
2025-06-02 10:21:43 +0600

    Add ShiftLog management with bulk import functionality and UI updates for supervisors' shift logs.

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,12 @@
 <?php
+
+use App\Http\Controllers\admin\ShiftLogController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'role:admin', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return view('admin.dashboard.index');
     })->name('admin.dashboard');
+    Route::resource('supervisors-shift-log', ShiftLogController::class);
+    Route::post('supervisors-shift-log/bulk-import', [ShiftLogController::class, 'bulkImport'])->name('supervisors-shift-log.bulk-import');
 });
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/test', function () {
     return view('test');
 })->middleware(['auth', 'verified'])->name('test');
 
-Route::get('admin/supervisors-shift-log', [SupervisorsShiftLog::class, 'index'])->name('supervisors-shift-log.index');
+//Route::get('admin/supervisors-shift-log', [SupervisorsShiftLog::class, 'index'])->name('supervisors-shift-log.index');
 Route::get('admin/supervisors-shift-log/{id}', [SupervisorsShiftLog::class, 'show'])->name('supervisors-shift-log.show');
